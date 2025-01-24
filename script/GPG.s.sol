0d6801eaceaf272a2360efb04008fdc14d5db359
Zach Obront <zobront@gmail.com>
2025-01-24 13:05:49 -0600

    rm all console imports

--- script/GPG.s.sol
+++ script/GPG.s.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/Console.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {GPGWallet} from "src/GPGWalletImpl.sol";
 
 contract GPGScript is Script {
